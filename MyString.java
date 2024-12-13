bb809be61e86084a7bd861ac937074910a72df6c
elishabarki <elisha.barki@gmail.com>
2024-12-13 10:25:24 +0200

    please again

--- MyString.java
+++ MyString.java
@@ -13,6 +13,16 @@ public class MyString {
         return true;
     }
 
+    public static int countChar(String str, char ch) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ch) {
+                count ++;
+            }
+        }
+        return count;
+    }
+    
     public static String spacedString(String str) {
         if (str == null || str.isEmpty()) {
             return "";
